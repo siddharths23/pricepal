4a6ad5f2de308e28ad2303985080f4ad722bc778
Siddharth Sankar <siddharthsankar23@gmail.com>
2024-05-31 02:30:06 -0700

    emailing functionality and cron jobs added

--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,54 @@
-import HeroCarosel from '@/components/HeroCarosel';
-import Searchbar from '@/components/Searchbar';
-import Image from 'next/image';
-import React from 'react'
+import HeroCarousel from "@/components/HeroCarosel"
+import Searchbar from "@/components/Searchbar"
+import Image from "next/image"
+import { getAllProducts } from "@/lib/actions"
+import ProductCard from "@/components/ProductCard"
 
-const Home = () => {
-  return(
-<>
-<section className='px-6 md:px-20 py-24 '>
-  <div className='flex max-xl:flex-col gap-16'>
-    <div className='flex flex-col justify-center'>
-     <p className='small-text'>Smart shopping starts here!</p>
-     <Image 
-     src="/assets/icons/arrow-right.svg"
-     alt="arrow-right"
-     width={26}
-     height={26}
-     />
-     <h1 className='head-text'>
-      Unleash the power of 
-      <span className='text-primary'> PricePal</span>
-     </h1>
-     <p className='mt-6'>
-     Powerful, self-serve product and growth analytics to help you convert, engage, and retain more.
-     </p>
+const Home = async () => {
+  const allProducts = await getAllProducts();
 
-     <Searchbar />
-    </div>
-    <HeroCarosel />
-  </div>
-</section>
-<section className='trending-section'>
-<h2 className='section-text'>Trending</h2>
-<div className='flex flex-wrap gap-x-8 gap-y-16'>
-  {['Apple Iphone','book','sneakers','ps5'].map((product)=>(
-    <div>{product}</div>
-  ))}
-</div>
-</section>
-</>
+  return (
+    <>
+      <section className="px-6 md:px-20 py-24">
+        <div className="flex max-xl:flex-col gap-16">
+          <div className="flex flex-col justify-center"> 
+            <p className="small-text">
+              Smart Shopping Starts Here:
+              <Image 
+                src="/assets/icons/arrow-right.svg"
+                alt="arrow-right"
+                width={16}
+                height={16}
+              />
+            </p>
+
+            <h1 className="head-text">
+              Unleash the Power of
+              <span className="text-primary"> PricePal!</span>
+            </h1>
+
+            <p className="mt-6">
+              Powerful, self-serve product and growth analytics to help you convert, engage, and retain more.
+            </p>
+
+            <Searchbar />
+          </div>
+
+          <HeroCarousel />
+        </div>
+      </section>
+
+      <section className="trending-section">
+        <h2 className="section-text">Trending</h2>
+
+        <div className="flex flex-wrap gap-x-8 gap-y-16">
+          {allProducts?.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
+        </div>
+      </section>
+    </>
   )
 }
 
-export default Home;
+export default Home
